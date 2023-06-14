09dd77f543a8c77cf23aa1cbbb17aad673be5b50
artydev <salve.didio@laposte.net>
2023-06-14 09:14:12 +0200

    add pages

---
+++ astro.config.mjs
@@ -0,0 +1,9 @@
+// astro.config.mjs
+import { defineConfig } from 'astro/config';
+import tailwind from "@astrojs/tailwind";
+
+// https://astro.build/config
+export default defineConfig({
+  integrations: [tailwind()]
+} // Here is you configure your app
+);
