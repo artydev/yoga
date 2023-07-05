d0f0589740a56b12153ed37b6c0d125c88599963
artydev <salve.didio@laposte.net>
2023-07-05 22:04:26 +0200

    restaure site

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,10 +2,9 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import compress from "astro-compress";
-import { astroImageTools } from "astro-imagetools";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), compress(), astroImageTools]
+  integrations: [tailwind(), compress()]
 } // Here is you configure your app
 );
