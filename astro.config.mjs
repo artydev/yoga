0870ddbc7648815fb3be97e8ae66078d618f374f
artydev <salve.didio@laposte.net>
2023-07-05 15:38:45 +0200

    add  AstroCompress, ImageTools

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,11 @@
 // astro.config.mjs
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
+import compress from "astro-compress";
+import { astroImageTools } from "astro-imagetools";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()]
+  integrations: [tailwind(), compress(), astroImageTools]
 } // Here is you configure your app
 );
