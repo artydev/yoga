d91ec9891c2e799b695337a30078663f6301b8e5
artydev <salve.didio@laposte.net>
2023-07-05 15:09:07 +0200

    adjust three cards

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,6 +2,16 @@
 module.exports = {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
+		borderWidth: {
+			DEFAULT: '1px',
+			'0': '0',
+			'2': '2px',
+			'3': '3px',
+			'4': '4px',
+			'6': '6px',
+			'8': '8px',
+			'20' : '20px'
+		  },
 		extend: {},
 	},
 	plugins: [],
